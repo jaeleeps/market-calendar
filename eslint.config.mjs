42056a656fe305a5a5f37da7a8431a134b4dc5bd
Jaeyoung Lee <jaeleeps@gmail.com>
2025-08-31 15:32:19 -0400

    chore: test and lint config fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,3 @@
-// eslint.config.js
 import js from '@eslint/js'
 import ts from '@typescript-eslint/eslint-plugin'
 import parser from '@typescript-eslint/parser'
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,7 +1,10 @@
+import {XNYS} from "../src/calendars/XNYS";
+
 console.log('Test file loaded')
 
 import { test } from '@japa/runner'
 import { add } from '../src'
+import {DateTime} from "luxon";
 
 test('add returns correct result', ({ assert }) => {
   assert.equal(add(2, 3), 5)
@@ -9,4 +12,20 @@ test('add returns correct result', ({ assert }) => {
 
 test('add fails intentionally', ({ assert }) => {
   assert.equal(add(2, 3), 99)
+})
+
+test.group('XNYS Calendar', () => {
+  test('market is open at 10:00 AM on normal day', ({ assert }) => {
+    const cal = new XNYS()
+    const dt = DateTime.fromISO('2024-07-02T10:00:00', { zone: 'America/New_York' })
+
+    assert.isTrue(cal.isOpenOnMinute(dt))
+  })
+
+  test('market is closed on July 4 (holiday)', ({ assert }) => {
+    const cal = new XNYS()
+    const dt = DateTime.fromISO('2024-07-04T10:00:00', { zone: 'America/New_York' })
+
+    assert.isFalse(cal.isOpenOnMinute(dt))
+  })
 })
